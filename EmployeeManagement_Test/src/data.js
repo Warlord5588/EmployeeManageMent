a676887f5985368ed5d5eecfcb439432d6287b2a
Weatphom Mamak <meawjow@gmail.com>
2023-03-23 10:57:57 +0700

    Add the existing project

---
+++ EmployeeManagement_Test/src/data.js
@@ -0,0 +1,212 @@
+export const employees = [
+  {
+    id: 1,
+    name: 'John Doe',
+    department: 'Marketing',
+    salary: 5000,
+    hiredDate: '2020-01-01'
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    department: 'Sales',
+    salary: 6000,
+    hiredDate: '2019-01-01'
+  },
+  {
+    id: 3,
+    name: 'Bob Smith',
+    department: 'Engineering',
+    salary: 7000,
+    hiredDate: '2018-01-01'
+  },
+  {
+    id: 4,
+    name: 'Mary Johnson',
+    department: 'HR',
+    salary: 4000,
+    hiredDate: '2021-01-01'
+  },
+  {
+    id: 5,
+    name: 'Sarah Williams',
+    department: 'Marketing',
+    salary: 5500,
+    hiredDate: '2020-02-01'
+  },
+  {
+    id: 6,
+    name: 'David Lee',
+    department: 'Sales',
+    salary: 6500,
+    hiredDate: '2019-03-01'
+  },
+  {
+    id: 7,
+    name: 'Tom Wilson',
+    department: 'Engineering',
+    salary: 7500,
+    hiredDate: '2018-04-01'
+  },
+  {
+    id: 8,
+    name: 'Amanda Brown',
+    department: 'HR',
+    salary: 4500,
+    hiredDate: '2021-02-01'
+  },
+  {
+    id: 9,
+    name: 'Kevin Davis',
+    department: 'Marketing',
+    salary: 5200,
+    hiredDate: '2020-03-01'
+  },
+  {
+    id: 10,
+    name: 'Linda Jones',
+    department: 'Sales',
+    salary: 6800,
+    hiredDate: '2019-04-01'
+  },
+  {
+    id: 11,
+    name: 'Steve White',
+    department: 'Engineering',
+    salary: 7600,
+    hiredDate: '2018-05-01'
+  },
+  {
+    id: 12,
+    name: 'Emily Garcia',
+    department: 'HR',
+    salary: 4200,
+    hiredDate: '2021-03-01'
+  },
+  {
+    id: 13,
+    name: 'Jacob Wilson',
+    department: 'Marketing',
+    salary: 5300,
+    hiredDate: '2020-04-01'
+  },
+  {
+    id: 14,
+    name: 'Hannah Martinez',
+    department: 'Sales',
+    salary: 6700,
+    hiredDate: '2019-05-01'
+  },
+  {
+    id: 15,
+    name: 'Michael Johnson',
+    department: 'Engineering',
+    salary: 7700,
+    hiredDate: '2018-06-01'
+  },
+  {
+    id: 16,
+    name: 'Olivia Brown',
+    department: 'HR',
+    salary: 4100,
+    hiredDate: '2021-04-01'
+  },
+  {
+    id: 17,
+    name: 'Ethan Davis',
+    department: 'Marketing',
+    salary: 5400,
+    hiredDate: '2020-05-01'
+  },
+  {
+    id: 18,
+    name: 'Olivia Davis',
+    department: 'Marketing',
+    salary: 5500,
+    hiredDate: '2021-06-01'
+  },
+  {
+    id: 19,
+    name: 'Ethan Brown',
+    department: 'Engineering',
+    salary: 7500,
+    hiredDate: '2017-11-01'
+  },
+  {
+    id: 20,
+    name: 'Sophie Miller',
+    department: 'Sales',
+    salary: 6200,
+    hiredDate: '2020-07-01'
+  },
+  {
+    id: 21,
+    name: 'Avery Wilson',
+    department: 'HR',
+    salary: 4200,
+    hiredDate: '2022-02-01'
+  },
+  {
+    id: 22,
+    name: 'Liam Anderson',
+    department: 'Engineering',
+    salary: 8000,
+    hiredDate: '2019-09-01'
+  },
+  {
+    id: 23,
+    name: 'Charlotte Taylor',
+    department: 'Marketing',
+    salary: 5300,
+    hiredDate: '2021-03-01'
+  },
+  {
+    id: 24,
+    name: 'Noah Martinez',
+    department: 'Sales',
+    salary: 6500,
+    hiredDate: '2018-08-01'
+  },
+  {
+    id: 25,
+    name: 'Amelia Hernandez',
+    department: 'HR',
+    salary: 4300,
+    hiredDate: '2020-10-01'
+  },
+  {
+    id: 26,
+    name: 'William Moore',
+    department: 'Engineering',
+    salary: 7700,
+    hiredDate: '2019-04-01'
+  },
+  {
+    id: 27,
+    name: 'Abigail Jackson',
+    department: 'Marketing',
+    salary: 5200,
+    hiredDate: '2022-01-01'
+  },
+  {
+    id: 28,
+    name: 'James Lee',
+    department: 'Sales',
+    salary: 6300,
+    hiredDate: '2018-12-01'
+  },
+  {
+    id: 29,
+    name: 'Evelyn Gonzalez',
+    department: 'HR',
+    salary: 4400,
+    hiredDate: '2020-06-01'
+  },
+  {
+    id: 30,
+    name: 'Oliver Perez',
+    department: 'Engineering',
+    salary: 7900,
+    hiredDate: '2019-02-01'
+  }
+]  
