42f2cf44191eca80af0d9ccbb518e967f4cb5f8d
Weatphom Mamak <meawjow@gmail.com>
2023-03-23 13:33:12 +0700

    Change component name

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <EmployeePage />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import EmployeePage from '@/components/EmployeeList.vue'
 </script>
